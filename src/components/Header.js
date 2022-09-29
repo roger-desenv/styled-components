e4de3da5405a6dab38c14a2e4360218f1d642bc0
Roger Costa <roger.desenv@gmail.com>
2022-09-29 17:12:02 -0300

    iniciando styled-components

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+const Box = styled.div`
+width: 100px;
+height: 100px;
+background-color: black;
+`
+
+function Header() {
+    return (
+      <>
+        <Box />
+      </>
+    )
+  }
+  
+  export default Header
+  
